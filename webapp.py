86ad7ffec4be1755ed05d289ff8a66c5e035c20e
Yashvi-Mervana <103818900+Yashvi-Mervana@users.noreply.github.com>
2023-10-24 19:17:40 +0530

    Update for thingspeak webapp.py

--- webapp.py
+++ webapp.py
@@ -1,7 +1,33 @@
-import numpy as np
-import pickle
 import streamlit as st
+import requests
+import pickle
+import numpy as np
 
+def update_text_boxes(data):
+    st.write(f"Temperature: {data.get('field1', 'N/A')}")
+    st.write(f"Heartrate: {data.get('field2', 'N/A')}")
+
+st.title("HEALTH MONITORING SYSTEM")
+
+# Button to trigger data update
+if st.button("Fetch Data"):
+    api_url = "https://api.thingspeak.com/channels/2095787/feeds.json?api_key=ZWF9Z0QVSFGLV02H&results=1"
+    
+    # Fetch data from the server
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        feeds = data.get("feeds", [])
+        
+        if feeds:
+            latest_data = feeds[0]  # Assuming the latest data is the first item in the list
+            update_text_boxes(latest_data)
+            st.success("Data updated successfully.")
+        else:
+            st.warning("No data found.")
+    else:
+        st.error(f"Failed to fetch data. Status code: {response.status_code}")
 
 loaded_model = pickle.load(open('trainedmmodel.sav', 'rb'))
 
